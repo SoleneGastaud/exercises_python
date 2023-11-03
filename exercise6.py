7987ff942986d18d7e09e2e9895bada4dcd53ea5
SoleneGastaud <solene.tab@gmail.com>
2023-11-03 15:22:10 +0100

    added a comment

---
+++ exercise6.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  3 12:56:06 2023
+
+@author: s_gastaud21
+"""
+
+a = 1
+r = 0.5
+s = 0
+k = 0
+
+while True:
+        s += a * r**k
+        k += 1
+        print(s, end = "")
+        if 
+        break 
